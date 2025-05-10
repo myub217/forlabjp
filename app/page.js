f83baa2b621d5451c1e9cbe22c7cebf6f2dbb18b
myub217 <myub25217@gmail.com>
2025-05-10 09:32:14 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+// app/page.js
+import Hero from '../components/Hero';
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      {/* คุณสามารถเพิ่มคอมโพเนนต์อื่น ๆ เช่น Services, Portfolio ฯลฯ */}
+    </main>
+  );
+}
