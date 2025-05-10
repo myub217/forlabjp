cc1fa747e5d5161fe2bbcb256c740557cc0a8a71
myub217 <myub25217@gmail.com>
2025-05-10 09:25:53 +0700

    Create App.js

---
+++ app/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Hero from './components/Hero';
+
+const App = () => {
+  return (
+    <div>
+      <Hero />
+      {/* คุณสามารถเพิ่มคอมโพเนนต์อื่น ๆ เช่น Services, Portfolio ฯลฯ */}
+    </div>
+  );
+};
+
+export default App;
